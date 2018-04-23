d108bcf8f00f1820ec8aff36d636909975838991
edmond120 <edmondwong888@msn.com>
2018-04-23 00:18:48 -0400

    a

--- shapes.py
+++ shapes.py
@@ -13,20 +13,26 @@ def make_torus(x, y, z, radius1, radius2, circles=150,edge=100):
 
 	points = edges * 2
 	translate = make_translate( x, y, z )
-	circle = Circle(radius2 - radius1,0,0,radius1,scale=1/points)
-	points = Matrix( list(circle[0:points:1/points]) )
+	circle_function = Circle(radius2 - radius1,0,0,radius1,scale=1/points)
+	circle = Matrix( list(circle_function[0:points:1/points]) )
 
 	#format points cuz the matrix only has x and y
-	for point in points:
+	for point in circle:
 		point.append(0)
 		point.append(1)
 
 	theta = (math.pi * 2) / circles
 
 	polygons = Matrix([])
-	rotY - make_rotY(circles)
+	rotY = make_rotY(theta)
 	for i in xrange(circles):
-		pass
+		next_circle = circle * rotY
+		p = 0;
+		end = len(circle)
+		while(p + 1 < end):
+			polygons.extend([circle[0],circle[1],next_circle[0]])
+			polygons.extend([next_circle[0],circle[1],next_circle[1])
+		
 
 class Torus(Matrix):
 	def __init__(self, x, y, z, radius1, radius2, \
